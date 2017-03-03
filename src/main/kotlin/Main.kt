e8a53476df050f2400974dfab96e93e82fec3535
Jérôme Mahuet <jerome.mahuet@gmail.com>
2017-03-03 22:48:08 +0100

    setup

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,65 @@
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.glfw.GLFWKeyCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.system.MemoryUtil.NULL
+
+class MyAss() {
+    var regex = 0
+    var bitchplease = 0
+
+    init {
+        regex = 4
+        bitchplease += 40
+
+
+    }
+
+}
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (!glfwInit()) {
+            throw IllegalStateException("Unable to initialize GLFW")
+        }
+
+        // Configure our window
+        glfwDefaultWindowHints()
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
+
+        val window = glfwCreateWindow(500, 300, "Hello World!", NULL, NULL)
+        if (window == NULL) {
+            throw RuntimeException("Failed to create the GLFW window")
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window)
+        // Enable v-sync
+        glfwSwapInterval(1)
+
+        // Make the window visible
+        glfwShowWindow(window)
+
+        GL.createCapabilities()
+        // Set the clear color
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+
+        val loop = fun () {
+            // Clear the framebuffer
+            glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+            // Swap the color buffers
+            glfwSwapBuffers(window)
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents()
+        }
+
+        while (true) {
+            loop()
+        }
+    }
+}
